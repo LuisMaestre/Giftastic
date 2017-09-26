8d4bdc8c7591e987c1d277fb551715021702b36a
Luis Maestre <luismaestre@luiss-mbp.attlocal.net>
2017-09-26 00:25:10 -0500

    2

--- javascript.js
+++ javascript.js
@@ -1,22 +1,50 @@
-$(function() {
-	populateButtons(searchArray,"searchButton","#ButtonsArea");
-  console.log("Page loaded");
+$(function(){
+  populateButtons(searchArray,'searchButton','#buttonsArea');
+  console.log("pageLoaded");
 })
 
-var searchArray= ["Dog","Cat","Bird"];
+
+var searchArray = ['Dog',"Cat","Bird"];
+
 
  function populateButtons(searchArray,classToAdd,areaToAddTo){
- 	$(areaToAddTo).empty();
- 	for(var i=0;i<searchArray.length;i++){
- 		var a= $("<button>");
- 		a.addClass(classToAdd);
- 		a.attr("data-type",searchArray[i]);
- 		a.text(searchArray[i]);
- 		$(areaToAddTo).append(a);
- 	}
+  $(areaToAddTo).empty();
+  for(var i=0;i<searchArray.length;i++){
+    var a = $('<button>');
+    a.addClass(classToAdd);
+    a.attr('data-type', searchArray[i]);
+    a.text(searchArray[i]);
+    $(areaToAddTo).append(a);
+  }
  }
- $(document).on('click',".searchButton",function(){
- 	var type = $(this).data("type");
- 	console.log(type); 
+$(document).on('click','.searchButton',function(){
+ $('#searches').empty();
+ var type = $(this).data('type');
+ var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=cmRgkRZ9uYwvvWt5w7cyMcjcFV44fQ9h&limit10';
+ $.ajax({url:queryURL,method:'GET'})
+    .done(function(response){
+      for(var i=0;i< response.data.length;i++){
+          var searchDiv = $('<div class="search-item">');
+          var rating = response.data[i].rating;
+          var p = $('<p>').text('Rating: '+rating);
+          var animated =response.data[i].images.fixed_height.url;
+          var still = response.data[i].images.fixed_height_still.url;
+          var image = $('<img>');
+          image.attr('src',still);
+          image.attr('data-still',still);
+          image.attr('data-animated',animated);
+          image.attr('data-state','still');
+          image.addClass('searchImage');
+          searchDiv.append(p);
+          searchDiv.append(image);
+         $("searches").append(searchDiv);
+          }
+      })
+})
 
- })
+$('#addSearch').on('click',function(){
+  var newSearch = $('input').eq(0).val();
+  searchArray.push(newSearch);
+    populateButtons(searchArray,'searchButton','#buttonsArea');
+  return false;
+})
